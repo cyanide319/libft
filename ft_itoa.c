da8b790610c588b6718a7398b4485506c092fb56
tbeaudoin <tristan-b@live.ca>
2022-04-08 14:12:01 -0400

    push2

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,69 @@
 /*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 18:41:17 by tbeaudoi          #+#    #+#             */
-/*   Updated: 2022/04/07 18:41:18 by tbeaudoi         ###   ########.fr       */
+/*   Updated: 2022/04/07 21:57:38 by tbeaudoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"libft.h"
+
+static char	*ft_putnum(char *res, unsigned int num, long int len)
+{
+	while (num > 0)
+	{
+		res[len] = (num % 10) + 48;
+		num /= 10;
+		len--;
+	}
+	return (res);
+}
+
+static int	ft_len(int nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb <= 0)
+		len = 1;
+	while (nb != 0)
+	{
+		len++;
+		nb /= 10;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nb)
+{
+	char	*str;
+	int		i;
+	long	n;
+
+	n = nb;
+	i = ft_len(n);
+	str = malloc(sizeof(char) * i + 1);
+	if (!str)
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = 48;
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+	else
+		nb = n;
+	str = ft_putnum(str, n, i);
+	return (str);
+}
+
+/*#include<stdio.h>
+
+int main(void)
+{
+	printf("%s\n", ft_itoa(123156));
+	return (0);
+}*/
