04b582c24e70d7da05b5d5a28a3336c0f4377e5c
tbeaudoin <tristan-b@live.ca>
2022-04-12 18:43:44 -0400

    strlcat2

---
+++ ft_strlcat2.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 22:05:52 by tbeaudoi          #+#    #+#             */
+/*   Updated: 2022/04/12 18:42:55 by tbeaudoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	destlen;
+
+	i = 0;
+	j = 0;
+	destlen = ft_strlen(dest);
+	i = destlen;
+	if (destlen < size - 1 && size > 0)
+	{
+		while (src[j] && destlen + j < size - 1)
+			dest[i++] = src[j++];
+		dest[i] = '\0';
+	}
+	if (destlen >= size)
+		destlen = size;
+	return (destlen + ft_strlen(src));
+}
